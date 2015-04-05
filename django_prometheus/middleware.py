82725490e5e023aef46fc496dbd3bd61d4419f46
Uriel Corfa <uriel@corfa.fr>
2015-04-05 13:58:53 -0700

    Counters are global state. Do not rely on Middleware.__init__ being called only once, because that's racy and the metrics may end up being defined multiple times. Instead, define them at import time.

--- django_prometheus/middleware.py
+++ django_prometheus/middleware.py
@@ -36,107 +36,101 @@ def PowersOf(logbase, count, lower=0, include_zero=True):
         return [0] + [logbase ** i for i in range(lower, count)] + [_INF]
 
 
-class BasePrometheusMiddleware(object):
-    """Common utilities for Prometheus middleware classes."""
-    def addCounter(self, name, description, *args, **kwargs):
-        return Counter(name, description, *args, **kwargs)
-
-    def addHistogram(self, name, description, *args, **kwargs):
-        return Histogram(name, description, *args, **kwargs)
+def addCounter(name, description, *args, **kwargs):
+    return Counter(name, description, *args, **kwargs)
 
 
-class PrometheusBeforeMiddleware(BasePrometheusMiddleware):
+def addHistogram(name, description, *args, **kwargs):
+    return Histogram(name, description, *args, **kwargs)
+
+
+requests_total = addCounter(
+    'django_http_requests_before_middlewares_total',
+    'Total count of requests before middlewares run.')
+responses_total = addCounter(
+    'django_http_responses_before_middlewares_total',
+    'Total count of responses before middlewares run.')
+requests_latency = addHistogram(
+    'django_http_requests_latency_including_middlewares_seconds',
+    ('Histogram of requests processing time (including middleware '
+     'processing time).'))
+
+
+class PrometheusBeforeMiddleware(object):
     """Monitoring middleware that should run before other middlewares."""
-    def __init__(self):
-        self.requests_total = self.addCounter(
-            'django_http_requests_before_middlewares_total',
-            'Total count of requests before middlewares run.')
-        self.responses_total = self.addCounter(
-            'django_http_responses_before_middlewares_total',
-            'Total count of responses before middlewares run.')
-        self.requests_latency = self.addHistogram(
-            'django_http_requests_latency_including_middlewares_seconds',
-            ('Histogram of requests processing time (including middleware '
-             'processing time).'))
-
     def process_request(self, request):
-        self.requests_total.inc()
+        requests_total.inc()
         request.prometheus_before_middleware_event = Time()
 
     def process_response(self, request, response):
-        self.responses_total.inc()
-        self.requests_latency.observe(TimeSince(
+        responses_total.inc()
+        requests_latency.observe(TimeSince(
             request.prometheus_before_middleware_event))
         return response
 
 
-class PrometheusAfterMiddleware(BasePrometheusMiddleware):
+requests_latency = addHistogram(
+    'django_http_requests_latency_seconds',
+    'Histogram of requests processing time.')
+# Set in process_request
+ajax_requests = addCounter(
+    'django_http_ajax_requests_total',
+    'Count of AJAX requests.')
+requests_by_method = addCounter(
+    'django_http_requests_total_by_method',
+    'Count of requests by method.',
+    ['method'])
+requests_by_transport = addCounter(
+    'django_http_requests_total_by_transport',
+    'Count of requests by transport.',
+    ['transport'])
+# Set in process_view
+requests_by_view = addCounter(
+    'django_http_requests_total_by_view',
+    'Count of requests by view.',
+    ['view_name'])
+requests_by_view_transport_method = addCounter(
+    'django_http_requests_total_by_view_transport_method',
+    'Count of requests by view, transport, method.',
+    ['view', 'transport', 'method'])
+requests_body_bytes = addHistogram(
+    'django_http_requests_body_total_bytes',
+    'Histogram of requests by body size.',
+    buckets=PowersOf(2, 30))
+# Set in process_template_response
+responses_by_templatename = addCounter(
+    'django_http_responses_total_by_templatename',
+    'Count of responses by template name.',
+    ['templatename'])
+# Set in process_response
+responses_by_status = addCounter(
+    'django_http_responses_total_by_status',
+    'Count of responses by status.',
+    ['status'])
+responses_body_bytes = addHistogram(
+    'django_http_responses_body_total_bytes',
+    'Histogram of responses by body size.',
+    buckets=PowersOf(2, 30))
+responses_by_charset = addCounter(
+    'django_http_responses_total_by_charset',
+    'Count of responses by charset. Django >=1.8 only.',
+    ['charset'])
+responses_streaming = addCounter(
+    'django_http_responses_streaming_total',
+    'Count of streaming responses.')
+# Set in process_exception
+exceptions_by_type = addCounter(
+    'django_http_exceptions_total_by_type',
+    'Count of exceptions by object type.',
+    ['type'])
+exceptions_by_view = addCounter(
+    'django_http_exceptions_total_by_view',
+    'Count of exceptions by view.',
+    ['view_name'])
+
+
+class PrometheusAfterMiddleware(object):
     """Monitoring middleware that should run after other middlewares."""
-    def __init__(self):
-        self.requests_latency = self.addHistogram(
-            'django_http_requests_latency_seconds',
-            'Histogram of requests processing time.')
-
-        # Set in process_request
-        self.ajax_requests = self.addCounter(
-            'django_http_ajax_requests_total',
-            'Count of AJAX requests.')
-        self.requests_by_method = self.addCounter(
-            'django_http_requests_total_by_method',
-            'Count of requests by method.',
-            ['method'])
-        self.requests_by_transport = self.addCounter(
-            'django_http_requests_total_by_transport',
-            'Count of requests by transport.',
-            ['transport'])
-
-        # Set in process_view
-        self.requests_by_view = self.addCounter(
-            'django_http_requests_total_by_view',
-            'Count of requests by view.',
-            ['view_name'])
-        self.requests_by_view_transport_method = self.addCounter(
-            'django_http_requests_total_by_view_transport_method',
-            'Count of requests by view, transport, method.',
-            ['view', 'transport', 'method'])
-        self.requests_body_bytes = self.addHistogram(
-            'django_http_requests_body_total_bytes',
-            'Histogram of requests by body size.',
-            buckets=PowersOf(2, 30))
-
-        # Set in process_template_response
-        self.responses_by_templatename = self.addCounter(
-            'django_http_responses_total_by_templatename',
-            'Count of responses by template name.',
-            ['templatename'])
-
-        # Set in process_response
-        self.responses_by_status = self.addCounter(
-            'django_http_responses_total_by_status',
-            'Count of responses by status.',
-            ['status'])
-        self.responses_body_bytes = self.addHistogram(
-            'django_http_responses_body_total_bytes',
-            'Histogram of responses by body size.',
-            buckets=PowersOf(2, 30))
-        self.responses_by_charset = self.addCounter(
-            'django_http_responses_total_by_charset',
-            'Count of responses by charset. Django >=1.8 only.',
-            ['charset'])
-        self.responses_streaming = self.addCounter(
-            'django_http_responses_streaming_total',
-            'Count of streaming responses.')
-
-        # Set in process_exception
-        self.exceptions_by_type = self.addCounter(
-            'django_http_exceptions_total_by_type',
-            'Count of exceptions by object type.',
-            ['type'])
-        self.exceptions_by_view = self.addCounter(
-            'django_http_exceptions_total_by_view',
-            'Count of exceptions by view.',
-            ['view_name'])
-
     def _transport(self, request):
         return 'https' if request.is_secure() else 'http'
 
@@ -150,42 +144,42 @@ class PrometheusAfterMiddleware(BasePrometheusMiddleware):
     def process_request(self, request):
         transport = self._transport(request)
         method = self._method(request)
-        self.requests_by_method.labels(method).inc()
-        self.requests_by_transport.labels(transport).inc()
+        requests_by_method.labels(method).inc()
+        requests_by_transport.labels(transport).inc()
         if request.is_ajax():
-            self.ajax_requests.inc()
-        self.requests_body_bytes.observe(len(request.body))
+            ajax_requests.inc()
+        requests_body_bytes.observe(len(request.body))
         request.prometheus_after_middleware_event = Time()
 
     def process_view(self, request, view_func, *view_args, **view_kwargs):
         transport = self._transport(request)
         method = self._method(request)
         name = request.resolver_match.view_name or '<unnamed view>'
-        self.requests_by_view.labels(name).inc()
-        self.requests_by_view_transport_method.labels(
+        requests_by_view.labels(name).inc()
+        requests_by_view_transport_method.labels(
             name, transport, method).inc()
 
     def process_template_response(self, request, response):
-        self.responses_by_templatename.labels(str(
+        responses_by_templatename.labels(str(
             response.template_name)).inc()
         return response
 
     def process_response(self, request, response):
-        self.responses_by_status.labels(str(response.status_code)).inc()
+        responses_by_status.labels(str(response.status_code)).inc()
         if hasattr(response, 'charset'):
-            self.responses_by_charset.labels(str(response.charset)).inc()
+            responses_by_charset.labels(str(response.charset)).inc()
         if response.streaming:
-            self.responses_streaming.inc()
-        self.responses_body_bytes.observe(len(response.content))
-        self.requests_latency.observe(TimeSince(
+            responses_streaming.inc()
+        responses_body_bytes.observe(len(response.content))
+        requests_latency.observe(TimeSince(
             request.prometheus_after_middleware_event))
         return response
 
     def process_exception(self, request, exception):
         name = request.resolver_match.view_name or '<unnamed view>'
-        self.exceptions_by_type.labels(type(exception).__name__).inc()
-        self.exceptions_by_view.labels(name).inc()
-        self.requests_latency.observe(TimeSince(
+        exceptions_by_type.labels(type(exception).__name__).inc()
+        exceptions_by_view.labels(name).inc()
+        requests_latency.observe(TimeSince(
             request.prometheus_after_middleware_event))
 
 
