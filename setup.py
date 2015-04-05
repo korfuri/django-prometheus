33eac9d11730e4733b5c27ae5a50d59d8d630819
Uriel Corfa <uriel@corfa.fr>
2015-04-05 02:19:11 -0700

    Clean up setup.py. Don't try to read README.md, it makes things more complicated than necessary.

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 import os
 from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name="django-prometheus",
-    version="0.0.1",
+    version="0.0.2",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
@@ -15,9 +11,9 @@ setup(
     license="Apache",
     keywords="django monitoring prometheus",
     url="http://github.com/korfuri/django-prometheus",
-    packages=['django_prometheus'],
-    long_description=read('README.md'),
-    install_requires=['prometheus_client'],
+    packages=["django_prometheus"],
+    long_description="See https://github.com/korfuri/django-prometheus",
+    install_requires=["prometheus_client"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: System :: Monitoring",
