81a35c396834667ba322456bac5abebe748e04f9
Uriel Corfa <uriel@corfa.fr>
2015-04-07 18:21:30 -0700

    Add a test for PowersOf.

--- tests/test_django_prometheus.py
+++ tests/test_django_prometheus.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import django_prometheus
+from django_prometheus.utils import PowersOf, _INF
 import unittest
 
 
-# TODO(korfuri): Add real tests. For now, this is just a placeholder
-# to set up a testing system.
 class DjangoPrometheusTest(unittest.TestCase):
-    def testNothing(self):
-        self.assertTrue(True)
+    def testPowersOf(self):
+        """Tests utils.PowersOf."""
+        self.assertEqual(
+            [0, 1, 2, 4, 8, _INF],
+            PowersOf(2, 4))
+        self.assertEqual(
+            [0, 3, 9, 27, 81, 243, _INF],
+            PowersOf(3, 5, lower=1))
+        self.assertEqual(
+            [1, 2, 4, 8, _INF],
+            PowersOf(2, 4, include_zero=False))
+        self.assertEqual(
+            [4, 8, 16, 32, 64, 128, _INF],
+            PowersOf(2, 6, lower=2, include_zero=False))
 
 
 if __name__ == 'main':
