a98204e305514210f31beb60e8a1fc57c2315aa3
Uriel Corfa <uriel@corfa.fr>
2015-04-11 00:44:07 -0700

    Release 0.0.10
    Adds database metrics.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ instructions.
 
 setup(
     name="django-prometheus",
-    version="0.0.9",
+    version="0.0.10",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
