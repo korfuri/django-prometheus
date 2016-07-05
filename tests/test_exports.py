32f80626f9fe22689ae534e56c80ebef10617feb
Jay Wineinger <jay.wineinger@gmail.com>
2016-07-05 15:00:47 -0500

    Mock HTTPServer in ExportTest.testPortRange to avoid attempts to allocate real ports

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
-from django_prometheus.exports import SetupPrometheusEndpointOnPortRange
+import socket
 import unittest
+
+from mock import patch, call, ANY, MagicMock
+
+from django_prometheus.exports import SetupPrometheusEndpointOnPortRange
 
 
 class ExportTest(unittest.TestCase):
-    def testPortRange(self):
+    @patch('django_prometheus.exports.HTTPServer')
+    def testPortRange(self, httpserver_mock):
+        httpserver_mock.side_effect = [socket.error, MagicMock()]
         port_range = [8000, 8001]
         SetupPrometheusEndpointOnPortRange(port_range)
-        SetupPrometheusEndpointOnPortRange(port_range)
+
+        expected_calls = [
+            call(('', 8000), ANY),
+            call(('', 8001), ANY),
+        ]
+        self.assertEqual(httpserver_mock.mock_calls, expected_calls)
+
 
 if __name__ == 'main':
     unittest.main()
