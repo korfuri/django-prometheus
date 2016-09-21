korfuri/django-prometheus

commit 432f1874ffde0ad120aa79e568086a1731d22aeb

Factor out most of the per-database code to a mixin.
Wrap calls to execute and executemany with a context manager that
counts exceptions by type (requires prometheus-client>=0.0.9).

django_prometheus/db/backends/mysql/base.py
@@ -1,7 +1,4 @@
-from django_prometheus.db.common import get_current_exception_type
-from django_prometheus.db import (
-    connections_total, execute_total, execute_many_total, errors_total,
-    connection_errors_total)
+from django_prometheus.db.common import DatabaseWrapperMixin
 from django.db.backends.mysql import base
 
 
@@ -10,49 +7,5 @@ class DatabaseFeatures(base.DatabaseFeatures):
     pass
 
 
-class DatabaseWrapper(base.DatabaseWrapper):
-    """Extends the DatabaseWrapper to count connections and cursors."""
-
-    def get_new_connection(self, *args, **kwargs):
-        connections_total.labels(self.alias, self.vendor).inc()
-        try:
-            return super(DatabaseWrapper, self).get_new_connection(
-                *args, **kwargs)
-        except:
-            connection_errors_total.labels(self.alias, self.vendor).inc()
-            raise
-
-    def create_cursor(self):
-        cursor = self.connection.cursor()
-        CursorWrapper = ExportingCursorWrapper(self.alias, self.vendor)
-        return CursorWrapper(cursor)
-
-
-def ExportingCursorWrapper(alias, vendor):
-    """Returns a CursorWrapper class that knows its database's alias and
-    vendor name.
-    """
-
-    class CursorWrapper(base.CursorWrapper):
-        """Extends the base CursorWrapper to count events."""
-
-        def execute(self, *args, **kwargs):
-            execute_total.labels(alias, vendor).inc()
-            try:
-                return super(CursorWrapper, self).execute(*args, **kwargs)
-            except:
-                errors_total.labels(
-                    alias, vendor, get_current_exception_type()).inc()
-                raise
-
-        def executemany(self, query, param_list, *args, **kwargs):
-            execute_total.labels(alias, vendor).inc(len(param_list))
-            execute_many_total.labels(alias, vendor).inc(len(param_list))
-            try:
-                return super(CursorWrapper, self).executemany(
-                    query=query, param_list=param_list, *args, **kwargs)
-            except:
-                errors_total.labels(
-                    alias, vendor, get_current_exception_type()).inc()
-                raise
-    return CursorWrapper
+class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+    CURSOR_CLASS = base.CursorWrapper

django_prometheus/db/backends/sqlite3/base.py
@@ -1,7 +1,4 @@
-from django_prometheus.db.common import get_current_exception_type
-from django_prometheus.db import (
-    connections_total, execute_total, execute_many_total, errors_total,
-    connection_errors_total)
+from django_prometheus.db.common import DatabaseWrapperMixin
 from django.db.backends.sqlite3 import base
 
 
@@ -10,48 +7,5 @@ class DatabaseFeatures(base.DatabaseFeatures):
     pass
 
 
-class DatabaseWrapper(base.DatabaseWrapper):
-    """Extends the DatabaseWrapper to count connections and cursors."""
-
-    def get_new_connection(self, *args, **kwargs):
-        connections_total.labels(self.alias, self.vendor).inc()
-        try:
-            return super(DatabaseWrapper, self).get_new_connection(
-                *args, **kwargs)
-        except:
-            connection_errors_total.labels(self.alias, self.vendor).inc()
-            raise
-
-    def create_cursor(self):
-        return self.connection.cursor(factory=ExportingCursorWrapper(
-            self.alias, self.vendor))
-
-
-def ExportingCursorWrapper(alias, vendor):
-    """Returns a CursorWrapper class that knows its database's alias and
-    vendor name.
-    """
-
-    class CursorWrapper(base.SQLiteCursorWrapper):
-        """Extends the base CursorWrapper to count events."""
-
-        def execute(self, *args, **kwargs):
-            execute_total.labels(alias, vendor).inc()
-            try:
-                return super(CursorWrapper, self).execute(*args, **kwargs)
-            except:
-                errors_total.labels(
-                    alias, vendor, get_current_exception_type()).inc()
-                raise
-
-        def executemany(self, query, param_list, *args, **kwargs):
-            execute_total.labels(alias, vendor).inc(len(param_list))
-            execute_many_total.labels(alias, vendor).inc(len(param_list))
-            try:
-                return super(CursorWrapper, self).executemany(
-                    query=query, param_list=param_list, *args, **kwargs)
-            except:
-                errors_total.labels(
-                    alias, vendor, get_current_exception_type()).inc()
-                raise
-    return CursorWrapper
+class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+    CURSOR_CLASS = base.SQLiteCursorWrapper

django_prometheus/db/common.py
@@ -1,9 +1,71 @@
+from django_prometheus.db import (
+    connections_total, execute_total, execute_many_total, errors_total,
+    connection_errors_total)
 import sys
 
 
-def get_current_exception_type():
-    """Returns the name of the type of the current exception."""
-    ex_type = sys.exc_info()[0]
-    if ex_type is None:
-        return '<no exception>'
-    return ex_type.__name__
+class ExceptionCounterByType(object):
+    """A context manager that counts exceptions by type.
+
+    Exceptions increment the provided counter, whose last label's name
+    must match the `type_label` argument.
+
+    In other words:
+
+    c = Counter('http_request_exceptions_total', 'Counter of exceptions',
+                ['method', 'type'])
+    with ExceptionCounterByType(c, extra_labels={'method': 'GET'}):
+        handle_get_request()
+    """
+
+    def __init__(self, counter, type_label='type', extra_labels=None):
+        self._counter = counter
+        self._type_label = type_label
+        self._labels = extra_labels
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, typ, value, traceback):
+        if typ is not None:
+            self._labels.update({self._type_label: typ.__name__})
+            self._counter.labels(self._labels).inc()
+
+
+class DatabaseWrapperMixin(object):
+    """Extends the DatabaseWrapper to count connections and cursors."""
+
+    def get_new_connection(self, *args, **kwargs):
+        connections_total.labels(self.alias, self.vendor).inc()
+        try:
+            return super(DatabaseWrapperMixin, self).get_new_connection(
+                *args, **kwargs)
+        except:
+            connection_errors_total.labels(self.alias, self.vendor).inc()
+            raise
+
+    def create_cursor(self):
+        return self.connection.cursor(factory=ExportingCursorWrapper(
+            self.CURSOR_CLASS, self.alias, self.vendor))
+
+
+def ExportingCursorWrapper(cursor_class, alias, vendor):
+    """Returns a CursorWrapper class that knows its database's alias and
+    vendor name.
+    """
+
+    class CursorWrapper(cursor_class):
+        """Extends the base CursorWrapper to count events."""
+
+        def execute(self, *args, **kwargs):
+            execute_total.labels(alias, vendor).inc()
+            with ExceptionCounterByType(errors_total, extra_labels={'alias': alias, 'vendor': vendor}):
+                return super(CursorWrapper, self).execute(*args, **kwargs)
+
+        def executemany(self, query, param_list, *args, **kwargs):
+            execute_total.labels(alias, vendor).inc(len(param_list))
+            execute_many_total.labels(alias, vendor).inc(len(param_list))
+            with ExceptionCounterByType(errors_total, extra_labels={'alias': alias, 'vendor': vendor}):
+                return super(CursorWrapper, self).executemany(
+                    query=query, param_list=param_list, *args, **kwargs)
+    return CursorWrapper

setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite="tests",
     long_description=LONG_DESCRIPTION,
     install_requires=[
-        "prometheus_client>=0.0.8",
+        "prometheus_client>=0.0.9",
         "pip-prometheus>=1.0.0",
     ],
     classifiers=[

commit 7092293a569c382dac4f2f9ac69b879ea4b500d1

Use the proper API to Python-MySQL.
The common mixin used for other databases uses an API established
across databases, but Python-MySQL differs. This was broken during the
refactoring in 432f1874ffde0ad120aa79e568086a1731d22aeb.

Fixes #24

django_prometheus/db/backends/mysql/base.py
@@ -1,4 +1,5 @@
-from django_prometheus.db.common import DatabaseWrapperMixin
+from django_prometheus.db.common import (
+    DatabaseWrapperMixin, ExportingCursorWrapper)
 from django.db.backends.mysql import base
 
 
@@ -9,3 +10,9 @@ class DatabaseFeatures(base.DatabaseFeatures):
 
 class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
     CURSOR_CLASS = base.CursorWrapper
+
+    def create_cursor(self):
+        cursor = self.connection.cursor()
+        CursorWrapper = ExportingCursorWrapper(
+            self.CURSOR_CLASS, self.alias, self.vendor)
+        return CursorWrapper(cursor)