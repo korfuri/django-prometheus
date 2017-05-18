deebbedf95626a1ab946c12b84ba66b0238c8def
Katharina Dankert <katharina@mautinoa.com>
2017-05-18 16:29:53 +0200

    Added postgis wrapper
    this is a variant of postgresql, so adapt the wrapper from PR #19

---
+++ django_prometheus/db/backends/postgis/base.py
@@ -0,0 +1,31 @@
+import django
+import psycopg2.extensions
+
+from django_prometheus.db.common import DatabaseWrapperMixin, \
+    ExportingCursorWrapper
+
+if django.VERSION >= (1, 9):
+    from django.db.backends.postgis import base
+else:
+    from django.db.backends.postgis_psycopg2 import base
+
+
+class DatabaseFeatures(base.DatabaseFeatures):
+    """Our database has the exact same features as the base one."""
+    pass
+
+
+class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+    def get_connection_params(self):
+        conn_params = super(DatabaseWrapper, self).get_connection_params()
+        conn_params['cursor_factory'] = ExportingCursorWrapper(
+            psycopg2.extensions.cursor,
+            self.alias,
+            self.vendor,
+        )
+        return conn_params
+
+    def create_cursor(self):
+        # cursor_factory is a kwarg to connect() so restore create_cursor()'s
+        # default behavior
+        return base.DatabaseWrapper.create_cursor(self)
