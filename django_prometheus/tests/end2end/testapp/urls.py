3e8d2e35cab89448e1414e9bc63375e26857cdf5
Uriel Corfa <korfuri@gmail.com>
2018-03-20 22:12:25 +0800

    Fix testapp's include of admin.site.urls to be compatible with Django 2

--- django_prometheus/tests/end2end/testapp/urls.py
+++ django_prometheus/tests/end2end/testapp/urls.py
@@ -1,6 +1,12 @@
+import django
 from django.conf.urls import include, url
 from django.contrib import admin
 from testapp import views
+
+if django.VERSION >= (1, 9, 0):
+    admin_urls = admin.site.urls
+else:
+    admin_urls = include(admin.site.urls)
 
 urlpatterns = [
     url(r'^$', views.index),
@@ -11,5 +17,5 @@ urlpatterns = [
     url(r'^newlawn/(?P<location>[a-zA-Z0-9 ]+)$', views.newlawn),
     url(r'^file$', views.file),
     url('', include('django_prometheus.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin_urls),
 ]
