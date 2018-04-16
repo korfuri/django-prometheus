bd21c2037cbfdebe036df2e9b635c19b36af2371
Geoffrey Beausire <geobeausire@gmail.com>
2018-04-16 15:26:41 +0200

    Added possibility to export metrics of several caches backends (#69)
    * Added metrics for filebased cache
    
    * Added more backends and more testings
    
    * Fix 2.7 compatibility
    
    * fix typos
    
    * Removed unwanted comments

---
+++ django_prometheus/cache/backends/memcached.py
@@ -0,0 +1,19 @@
+from django.core.cache.backends import memcached
+from django_prometheus.cache.metrics import (
+    django_cache_get_total, django_cache_hits_total, django_cache_misses_total)
+
+
+class MemcachedCache(memcached.MemcachedCache):
+    """Inherit memcached to add metrics about hit/miss ratio"""
+
+    def get(self, key, default=None, version=None):
+        django_cache_get_total.labels(backend='memcached').inc()
+        cached = super(MemcachedCache, self).get(
+            key, default=None, version=None)
+        if cached is not None:
+            django_cache_hits_total.labels(
+                backend='memcached').inc()
+        else:
+            django_cache_misses_total.labels(
+                backend='memcached').inc()
+        return cached
---
+++ django_prometheus/cache/metrics.py
@@ -0,0 +1,8 @@
+from prometheus_client import Counter
+
+django_cache_get_total = Counter('django_cache_get_total',
+                                 'Total get requests on cache', ['backend'])
+django_cache_hits_total = Counter('django_cache_get_hits_total',
+                                  'Total hits on cache', ['backend'])
+django_cache_misses_total = Counter('django_cache_get_misses_total',
+                                    'Total misses on cache', ['backend'])
