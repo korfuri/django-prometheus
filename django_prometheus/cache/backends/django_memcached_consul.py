6c523c76a0a80e47eeb5ab6d23e205481af2e778
Geoffrey Beausire <g.beausire@criteo.com>
2018-05-29 16:44:11 +0200

    Added compatibility with pypy

--- django_prometheus/cache/backends/django_memcached_consul.py
+++ django_prometheus/cache/backends/django_memcached_consul.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from django_memcached_consul import memcached
 from django_prometheus.cache.metrics import (
     django_cache_get_total, django_cache_hits_total, django_cache_misses_total)
