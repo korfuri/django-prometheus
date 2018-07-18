71a00211282a8e08a5eebf2acc6baa607e38dfcb
Uriel Corfa <korfuri@gmail.com>
2018-07-18 20:30:22 +0800

    Release 1.0.15

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ instructions.
 
 setup(
     name="django-prometheus",
-    version="1.0.14",
+    version="1.0.15",
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
     description=(
