33b632f2d01bb0e3321749579845d1530b35b957
Pawel Dudzinski <pawel.dudzinski@saucelabs.com>
2019-01-08 18:05:12 +0100

    Update

--- django_prometheus/db/__init__.py
+++ django_prometheus/db/__init__.py
@@ -1,11 +1,4 @@
 # Import all metrics
-__all__ = [
-    'Counter',
-    'connection_errors_total',
-    'connections_total',
-    'errors_total',
-    'execute_many_total',
-    'execute_total']
 from django_prometheus.db.metrics import (
     Counter,
     connection_errors_total,
@@ -13,3 +6,10 @@ from django_prometheus.db.metrics import (
     errors_total,
     execute_many_total,
     execute_total)
+__all__ = [
+    'Counter',
+    'connection_errors_total',
+    'connections_total',
+    'errors_total',
+    'execute_many_total',
+    'execute_total']
