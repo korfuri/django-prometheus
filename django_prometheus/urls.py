7a066f8a5c0213c555323bba0300caccbabfd321
David Guerrero <david.guerrero@prezi.com>
2019-03-19 13:55:27 +0100

    Fixing pep8

--- django_prometheus/urls.py
+++ django_prometheus/urls.py
@@ -1,11 +1,10 @@
 import django
+from django_prometheus import exports
 
 if django.VERSION < (1, 4, 0):
     from django.conf.urls.defaults import url
 else:
     from django.conf.urls import url
-
-from django_prometheus import exports
 
 
 urlpatterns = [
