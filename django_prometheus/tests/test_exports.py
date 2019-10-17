565eb349399d8c65fbb919596ad6bb0143553bc7
Sean Sawyer <seancsawyer@gmail.com>
2019-10-17 15:45:35 -0400

    Return the port chosen from port range in export setup

--- django_prometheus/tests/test_exports.py
+++ django_prometheus/tests/test_exports.py
@@ -9,16 +9,32 @@ from mock import patch, call, ANY, MagicMock
 
 class ExportTest(unittest.TestCase):
     @patch('django_prometheus.exports.HTTPServer')
-    def testPortRange(self, httpserver_mock):
+    def testPortRangeAvailable(self, httpserver_mock):
+        """Test port range setup with an available port."""
         httpserver_mock.side_effect = [socket.error, MagicMock()]
         port_range = [8000, 8001]
-        SetupPrometheusEndpointOnPortRange(port_range)
+        port_chosen = SetupPrometheusEndpointOnPortRange(port_range)
 
         expected_calls = [
             call(('', 8000), ANY),
             call(('', 8001), ANY),
         ]
         self.assertEqual(httpserver_mock.mock_calls, expected_calls)
+        self.assertEqual(port_chosen, 8001)
+
+    @patch('django_prometheus.exports.HTTPServer')
+    def testPortRangeUnavailable(self, httpserver_mock):
+        """Test port range setup with no available ports."""
+        httpserver_mock.side_effect = [socket.error, socket.error]
+        port_range = [8000, 8001]
+        port_chosen = SetupPrometheusEndpointOnPortRange(port_range)
+
+        expected_calls = [
+            call(('', 8000), ANY),
+            call(('', 8001), ANY),
+        ]
+        self.assertEqual(httpserver_mock.mock_calls, expected_calls)
+        self.assertIsNone(port_chosen)
 
 
 if __name__ == 'main':
