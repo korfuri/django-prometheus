e14a7b75e101aa6bb1525a6a148c4472962954f6
Asher Foa <1268088+asherf@users.noreply.github.com>
2019-10-18 12:06:25 -0700

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
