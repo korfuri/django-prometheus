cc88d6fda762ae005ea034dffa72fe9fa59c32ec
Asher Foa <1268088+asherf@users.noreply.github.com>
2019-11-15 22:40:03 -0800

    Configure logging in end 2 end test app

--- django_prometheus/tests/end2end/testapp/settings.py
+++ django_prometheus/tests/end2end/testapp/settings.py
@@ -180,3 +180,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = "/static/"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {"django": {"handlers": ["console"], "level": "INFO"}},
+}
