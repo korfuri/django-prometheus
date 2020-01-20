e6e6eb6b784f19b7748e649d8b5e7828e62a9bac
Asher Foa <1268088+asherf@users.noreply.github.com>
2020-01-20 15:15:02 -0800

    Use README.md as a long description

--- setup.py
+++ setup.py
@@ -1,41 +1,37 @@
 import re
 from setuptools import find_packages, setup
 
-LONG_DESCRIPTION = """Django-Prometheus
+with open("README.md") as fl:
+    LONG_DESCRIPTION = fl.read()
 
-This library contains code to expose some monitoring metrics relevant
-to Django internals so they can be monitored by Prometheus.io.
-
-See https://github.com/korfuri/django-prometheus for usage
-instructions.
-"""
 
 def get_version():
-    version_file = open('django_prometheus/__init__.py', 'r').read()
-    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', version_file, re.MULTILINE)
+    version_file = open("django_prometheus/__init__.py", "r").read()
+    version_match = re.search(
+        r'^__version__ = [\'"]([^\'"]*)[\'"]', version_file, re.MULTILINE
+    )
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="django-prometheus",
     version=get_version(),
     author="Uriel Corfa",
     author_email="uriel@corfa.fr",
-    description=(
-        "Django middlewares to monitor your application with Prometheus.io."),
+    description=("Django middlewares to monitor your application with Prometheus.io."),
     license="Apache",
     keywords="django monitoring prometheus",
     url="http://github.com/korfuri/django-prometheus",
     packages=find_packages(),
     test_suite="django_prometheus.tests",
     long_description=LONG_DESCRIPTION,
-    tests_require=['pytest', 'pytest-django'],
+    long_description_content_type="text/markdown",
+    tests_require=["pytest", "pytest-django"],
     setup_requires=["pytest-runner"],
     options={"bdist_wheel": {"universal": "1"}},
-    install_requires=[
-        "prometheus-client>=0.7",
-    ],
+    install_requires=["prometheus-client>=0.7",],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
