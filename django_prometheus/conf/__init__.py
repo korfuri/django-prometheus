8291e9a5c64fdb0d12490f599d6ff4b47105dd69
Asher Foa <asher@toolchain.com>
2020-03-31 21:04:09 -0700

    only use django settings

--- django_prometheus/conf/__init__.py
+++ django_prometheus/conf/__init__.py
@@ -6,5 +6,3 @@ NAMESPACE = ""
 
 if settings.configured:
     NAMESPACE = getattr(settings, "PROMETHEUS_METRIC_NAMESPACE", NAMESPACE)
-
-NAMESPACE = os.getenv("PROMETHEUS_METRIC_NAMESPACE", NAMESPACE)
