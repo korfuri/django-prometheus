8eebc39275f23444050de22e6ab2fb1857a5acb0
Alisson Claudino <alisson.claudino@azion.com>
2020-03-31 21:23:17 -0300

    fix: inject settings if it's configured

--- django_prometheus/conf/__init__.py
+++ django_prometheus/conf/__init__.py
@@ -2,5 +2,9 @@ import os
 
 from django.conf import settings
 
-APP_NAME = getattr(settings, 'METRICS_APP_NAME', 'django')
+APP_NAME = "django"
+
+if settings.configured:
+    APP_NAME = getattr(settings, 'METRICS_APP_NAME', APP_NAME)
+
 APP_NAME = os.getenv('METRICS_APP_NAME', APP_NAME)
