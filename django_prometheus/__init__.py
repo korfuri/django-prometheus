80c67930959df6b5dad5309623aabe1ce134786f
lowit <ea@lowit.ru>
2021-06-08 13:38:14 +0300

    patch-1: fixes import sort

--- django_prometheus/__init__.py
+++ django_prometheus/__init__.py
@@ -2,11 +2,11 @@
 
 https://github.com/korfuri/django-prometheus
 """
-import django
 
 # Import all files that define metrics. This has the effect that
 # `import django_prometheus` will always instantiate all metric
 # objects right away.
+import django
 from django_prometheus import middleware, models
 
 __all__ = ["middleware", "models", "pip_prometheus"]
