779dcb7a3f92391d4fd43c8a7ffbeda95d0512ea
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-12-08 18:26:04 -0800

    fix formatting

--- django_prometheus/exports.py
+++ django_prometheus/exports.py
@@ -115,8 +115,10 @@ def ExportToDjangoView(request):
 
     You can use django_prometheus.urls to map /metrics to this view.
     """
-    if ("PROMETHEUS_MULTIPROC_DIR" in os.environ
-            or "prometheus_multiproc_dir" in os.environ):
+    if (
+        "PROMETHEUS_MULTIPROC_DIR" in os.environ
+        or "prometheus_multiproc_dir" in os.environ
+    ):
         registry = prometheus_client.CollectorRegistry()
         multiprocess.MultiProcessCollector(registry)
     else:
