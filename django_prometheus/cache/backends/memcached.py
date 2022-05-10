c7bf0df42aa4c40bb253e5fce7479c82474ed0c3
tneuct <63791831+tneuct@users.noreply.github.com>
2022-05-10 15:00:48 +0200

    Add support for all cache backends of Django >=3.2

--- django_prometheus/cache/backends/memcached.py
+++ django_prometheus/cache/backends/memcached.py
@@ -1,3 +1,4 @@
+from django import VERSION as DJANGO_VERSION
 from django.core.cache.backends import memcached
 
 from django_prometheus.cache.metrics import (
@@ -7,9 +8,7 @@ from django_prometheus.cache.metrics import (
 )
 
 
-class MemcachedCache(memcached.PyLibMCCache):
-    """Inherit memcached to add metrics about hit/miss ratio"""
-
+class MemcachedPrometheusCacheMixin:
     def get(self, key, default=None, version=None):
         django_cache_get_total.labels(backend="memcached").inc()
         cached = super().get(key, default=None, version=version)
@@ -18,3 +17,22 @@ class MemcachedCache(memcached.PyLibMCCache):
         else:
             django_cache_misses_total.labels(backend="memcached").inc()
         return cached or default
+
+
+class MemcachedCache(MemcachedPrometheusCacheMixin, memcached.MemcachedCache):
+    """Inherit memcached to add metrics about hit/miss ratio"""
+
+    pass
+
+
+if DJANGO_VERSION >= (3, 2):
+
+    class PyLibMCCache(MemcachedPrometheusCacheMixin, memcached.PyLibMCCache):
+        """Inherit memcached to add metrics about hit/miss ratio"""
+
+        pass
+
+    class PyMemcacheCache(MemcachedPrometheusCacheMixin, memcached.PyMemcacheCache):
+        """Inherit memcached to add metrics about hit/miss ratio"""
+
+        pass
