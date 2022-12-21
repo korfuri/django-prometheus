23dd78bf1feb725de2395c8808c46fce088a25e8
AleksaC <aleksacukovic1@gmail.com>
2022-12-22 00:17:24 +0100

    Move latency buckets setting to django_prometheus.conf and use it for django_http_requests_latency_including_middlewares_seconds and django_db_query_duration_seconds histograms

--- django_prometheus/conf/__init__.py
+++ django_prometheus/conf/__init__.py
@@ -2,5 +2,26 @@ from django.conf import settings
 
 NAMESPACE = ""
 
+PROMETHEUS_LATENCY_BUCKETS = (
+    0.01,
+    0.025,
+    0.05,
+    0.075,
+    0.1,
+    0.25,
+    0.5,
+    0.75,
+    1.0,
+    2.5,
+    5.0,
+    7.5,
+    10.0,
+    25.0,
+    50.0,
+    75.0,
+    float("inf"),
+)
+
 if settings.configured:
     NAMESPACE = getattr(settings, "PROMETHEUS_METRIC_NAMESPACE", NAMESPACE)
+    PROMETHEUS_LATENCY_BUCKETS = getattr(settings, "PROMETHEUS_LATENCY_BUCKETS", PROMETHEUS_LATENCY_BUCKETS)
