50e04004829891606f1a761bbf8d522b7621404b
Jens Langhammer <jens.langhammer@beryju.org>
2023-04-13 17:43:57 +0300

    add support for django < 4.2

--- django_prometheus/db/backends/postgis/base.py
+++ django_prometheus/db/backends/postgis/base.py
@@ -1,17 +1,38 @@
-from django.contrib.gis.db.backends.postgis import base
-from django.db.backends.postgresql.base import Cursor
-from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
+from django import VERSION
 
+if VERSION < (4, 2):
+    import psycopg2.extensions
+    from django.contrib.gis.db.backends.postgis import base
 
-class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
-    def get_connection_params(self):
-        conn_params = super().get_connection_params()
-        conn_params["cursor_factory"] = ExportingCursorWrapper(
-            Cursor, "postgis", self.vendor
-        )
-        return conn_params
+    from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
 
-    def create_cursor(self, name=None):
-        # cursor_factory is a kwarg to connect() so restore create_cursor()'s
-        # default behavior
-        return base.DatabaseWrapper.create_cursor(self, name=name)
+    class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+        def get_connection_params(self):
+            conn_params = super().get_connection_params()
+            conn_params["cursor_factory"] = ExportingCursorWrapper(
+                psycopg2.extensions.cursor, "postgis", self.vendor
+            )
+            return conn_params
+
+        def create_cursor(self, name=None):
+            # cursor_factory is a kwarg to connect() so restore create_cursor()'s
+            # default behavior
+            return base.DatabaseWrapper.create_cursor(self, name=name)
+
+else:
+    from django.contrib.gis.db.backends.postgis import base
+    from django.db.backends.postgresql.base import Cursor
+    from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
+
+    class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+        def get_connection_params(self):
+            conn_params = super().get_connection_params()
+            conn_params["cursor_factory"] = ExportingCursorWrapper(
+                Cursor, "postgis", self.vendor
+            )
+            return conn_params
+
+        def create_cursor(self, name=None):
+            # cursor_factory is a kwarg to connect() so restore create_cursor()'s
+            # default behavior
+            return base.DatabaseWrapper.create_cursor(self, name=name)
