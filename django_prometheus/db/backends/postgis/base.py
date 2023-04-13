9d733e02b67d934765de5731ca86c8d933ddd86f
Jens Langhammer <jens.langhammer@beryju.org>
2023-04-13 12:20:10 +0300

    add django 4.2 support

--- django_prometheus/db/backends/postgis/base.py
+++ django_prometheus/db/backends/postgis/base.py
@@ -1,13 +1,14 @@
-import psycopg2.extensions
 from django.contrib.gis.db.backends.postgis import base
-
+from django.db.backends.postgresql.base import Cursor
 from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
 
 
 class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
     def get_connection_params(self):
         conn_params = super().get_connection_params()
-        conn_params["cursor_factory"] = ExportingCursorWrapper(psycopg2.extensions.cursor, "postgis", self.vendor)
+        conn_params["cursor_factory"] = ExportingCursorWrapper(
+            Cursor, "postgis", self.vendor
+        )
         return conn_params
 
     def create_cursor(self, name=None):
