6e45fa967d76b824d395ccbad518c89b71a9c994
Jens Langhammer <jens.langhammer@beryju.org>
2023-04-13 17:43:57 +0300

    add support for django < 4.2

--- django_prometheus/db/backends/postgresql/base.py
+++ django_prometheus/db/backends/postgresql/base.py
@@ -1,20 +1,44 @@
-from django.db.backends.postgresql import base
-from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
+from django import VERSION
 
+if VERSION < (4, 2):
+    import psycopg2.extensions
+    from django.db.backends.postgresql import base
 
-class DatabaseFeatures(base.DatabaseFeatures):
-    """Our database has the exact same features as the base one."""
+    from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
 
-    pass
+    class DatabaseFeatures(base.DatabaseFeatures):
+        """Our database has the exact same features as the base one."""
 
+        pass
 
-class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
-    def get_connection_params(self):
-        conn_params = super().get_connection_params()
-        conn_params["cursor_factory"] = ExportingCursorWrapper(base.Cursor, self.alias, self.vendor)
-        return conn_params
+    class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+        def get_connection_params(self):
+            conn_params = super().get_connection_params()
+            conn_params["cursor_factory"] = ExportingCursorWrapper(psycopg2.extensions.cursor, self.alias, self.vendor)
+            return conn_params
 
-    def create_cursor(self, name=None):
-        # cursor_factory is a kwarg to connect() so restore create_cursor()'s
-        # default behavior
-        return base.DatabaseWrapper.create_cursor(self, name=name)
+        def create_cursor(self, name=None):
+            # cursor_factory is a kwarg to connect() so restore create_cursor()'s
+            # default behavior
+            return base.DatabaseWrapper.create_cursor(self, name=name)
+
+else:
+    from django.db.backends.postgresql import base
+
+    from django_prometheus.db.common import DatabaseWrapperMixin, ExportingCursorWrapper
+
+    class DatabaseFeatures(base.DatabaseFeatures):
+        """Our database has the exact same features as the base one."""
+
+        pass
+
+    class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
+        def get_connection_params(self):
+            conn_params = super().get_connection_params()
+            conn_params["cursor_factory"] = ExportingCursorWrapper(base.Cursor, self.alias, self.vendor)
+            return conn_params
+
+        def create_cursor(self, name=None):
+            # cursor_factory is a kwarg to connect() so restore create_cursor()'s
+            # default behavior
+            return base.DatabaseWrapper.create_cursor(self, name=name)
