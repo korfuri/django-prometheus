3713eda414943f8379d3a99268e218e154b2a038
Jens Langhammer <jens.langhammer@beryju.org>
2023-04-16 12:32:57 +0300

    add django 4.2 to compat list

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
         "Topic :: System :: Monitoring",
         "License :: OSI Approved :: Apache Software License",
     ],
