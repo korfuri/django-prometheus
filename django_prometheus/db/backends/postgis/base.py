7315cfa55b93dc21b172b60cf24bdea88a6a8162
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-04-21 15:22:43 -0400

    more logging

--- django_prometheus/db/backends/postgis/base.py
+++ django_prometheus/db/backends/postgis/base.py
@@ -10,9 +10,11 @@ class DatabaseWrapper(DatabaseWrapperMixin, base.DatabaseWrapper):
         conn_params["cursor_factory"] = ExportingCursorWrapper(
             psycopg2.extensions.cursor, "postgis", self.vendor
         )
+        print(f"**** connection for PostGIS: {conn_params!r}")
         return conn_params
 
     def create_cursor(self, name=None):
         # cursor_factory is a kwarg to connect() so restore create_cursor()'s
         # default behavior
+        print("**** Create cursur for PostGIS **** ")
         return base.DatabaseWrapper.create_cursor(self, name=name)
