1608d21d1ae3bd329ebab113b69c9293143ede50
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-04-24 11:58:37 -0400

    Add support for python 3.12

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
