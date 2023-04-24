988b132f6848988cf3b8895c70f2fa105cf0e66e
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-04-24 14:11:56 -0400

    Remove old django version leftovers

--- django_prometheus/__init__.py
+++ django_prometheus/__init__.py
@@ -6,8 +6,6 @@ https://github.com/korfuri/django-prometheus
 # Import all files that define metrics. This has the effect that
 # `import django_prometheus` will always instantiate all metric
 # objects right away.
-import django
-
 from django_prometheus import middleware, models
 
 __all__ = ["middleware", "models", "pip_prometheus"]
@@ -20,6 +18,3 @@ try:
 except ImportError:
     # If people don't have pip, don't export anything.
     pass
-
-if django.VERSION < (3, 1):
-    default_app_config = "django_prometheus.apps.DjangoPrometheusConfig"
