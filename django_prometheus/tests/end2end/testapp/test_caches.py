efb75297567aa95786914898bd273247697cf4e8
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-04-28 16:26:31 -0400

    more pytest

--- django_prometheus/tests/end2end/testapp/test_caches.py
+++ django_prometheus/tests/end2end/testapp/test_caches.py
@@ -1,3 +1,4 @@
+import pytest
 from django.core.cache import caches
 from django.test import TestCase
 from redis import RedisError
@@ -8,45 +9,33 @@ from django_prometheus.testutils import assert_metric_equal, get_metric
 class TestCachesMetrics(TestCase):
     """Test django_prometheus.caches metrics."""
 
-    def test_counters(self):
-        supported_caches = [
-            "memcached.PyLibMCCache",
-            "memcached.PyMemcacheCache",
-            "filebased",
-            "locmem",
-            "redis",
-        ]
-
+    @pytest.mark.parametrize(
+        "supported_cache", ["memcached.PyLibMCCache", "memcached.PyMemcacheCache", "filebased", "locmem", "redis"]
+    )
+    def test_counters(self, supported_cache):
         # Note: those tests require a memcached server running
-        for supported_cache in supported_caches:
-            tested_cache = caches[supported_cache]
-            backend = supported_cache.split(".")[0]
-
-            total_before = get_metric("django_cache_get_total", backend=backend) or 0
-            hit_before = get_metric("django_cache_get_hits_total", backend=backend) or 0
-            miss_before = get_metric("django_cache_get_misses_total", backend=backend) or 0
-
-            tested_cache.set("foo1", "bar")
-            tested_cache.get("foo1")
-            tested_cache.get("foo1")
-            tested_cache.get("foofoo")
-
-            result = tested_cache.get("foofoo", default="default")
-
-            assert result == "default"
-
-            assert_metric_equal(total_before + 4, "django_cache_get_total", backend=backend)
-            assert_metric_equal(hit_before + 2, "django_cache_get_hits_total", backend=backend)
-            assert_metric_equal(
-                miss_before + 2,
-                "django_cache_get_misses_total",
-                backend=backend,
-            )
+        tested_cache = caches[supported_cache]
+        backend = supported_cache.split(".")[0]
+        total_before = get_metric("django_cache_get_total", backend=backend) or 0
+        hit_before = get_metric("django_cache_get_hits_total", backend=backend) or 0
+        miss_before = get_metric("django_cache_get_misses_total", backend=backend) or 0
+        tested_cache.set("foo1", "bar")
+        tested_cache.get("foo1")
+        tested_cache.get("foo1")
+        tested_cache.get("foofoo")
+        result = tested_cache.get("foofoo", default="default")
+        assert result == "default"
+        assert_metric_equal(total_before + 4, "django_cache_get_total", backend=backend)
+        assert_metric_equal(hit_before + 2, "django_cache_get_hits_total", backend=backend)
+        assert_metric_equal(
+            miss_before + 2,
+            "django_cache_get_misses_total",
+            backend=backend,
+        )
 
     def test_redis_cache_fail(self):
         # Note: test use fake service config (like if server was stopped)
         supported_cache = "redis"
-
         total_before = get_metric("django_cache_get_total", backend=supported_cache) or 0
         fail_before = get_metric("django_cache_get_fail_total", backend=supported_cache) or 0
         hit_before = get_metric("django_cache_get_hits_total", backend=supported_cache) or 0
@@ -61,7 +50,7 @@ class TestCachesMetrics(TestCase):
         assert_metric_equal(fail_before + 1, "django_cache_get_fail_total", backend=supported_cache)
 
         tested_cache = caches["stopped_redis"]
-        with self.assertRaises(RedisError):
+        with pytest.raises(RedisError):
             tested_cache.get("foo1")
 
         assert_metric_equal(hit_before, "django_cache_get_hits_total", backend=supported_cache)
@@ -69,21 +58,13 @@ class TestCachesMetrics(TestCase):
         assert_metric_equal(total_before + 2, "django_cache_get_total", backend=supported_cache)
         assert_metric_equal(fail_before + 2, "django_cache_get_fail_total", backend=supported_cache)
 
-    def test_cache_version_support(self):
-        supported_caches = [
-            "memcached.PyLibMCCache",
-            "memcached.PyMemcacheCache",
-            "filebased",
-            "locmem",
-            "redis",
-        ]
-
+    @pytest.mark.parametrize(
+        "supported_cache", ["memcached.PyLibMCCache", "memcached.PyMemcacheCache", "filebased", "locmem", "redis"]
+    )
+    def test_cache_version_support(self, supported_cache):
         # Note: those tests require a memcached server running
-        for supported_cache in supported_caches:
-            tested_cache = caches[supported_cache]
-
-            tested_cache.set("foo1", "bar v.1", version=1)
-            tested_cache.set("foo1", "bar v.2", version=2)
-
-            assert "bar v.1" == tested_cache.get("foo1", version=1)
-            assert "bar v.2" == tested_cache.get("foo1", version=2)
+        tested_cache = caches[supported_cache]
+        tested_cache.set("foo1", "bar v.1", version=1)
+        tested_cache.set("foo1", "bar v.2", version=2)
+        assert "bar v.1" == tested_cache.get("foo1", version=1)
+        assert "bar v.2" == tested_cache.get("foo1", version=2)
