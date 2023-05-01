650b4aafb54570cd393775b8d0e324b3399b1e61
Asher Foa <1268088+asherf@users.noreply.github.com>
2023-05-01 15:55:49 -0400

    Back to 2.4.0 dev releases

--- django_prometheus/__init__.py
+++ django_prometheus/__init__.py
@@ -10,7 +10,7 @@ from django_prometheus import middleware, models
 
 __all__ = ["middleware", "models", "pip_prometheus"]
 
-__version__ = "2.3.0"
+__version__ = "2.4.0.dev0"
 
 # Import pip_prometheus to export the pip metrics automatically.
 try:
