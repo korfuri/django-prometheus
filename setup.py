253c66a9c2985ffc14defa647de830a3d9880a9a
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-06-06 22:19:44 +0300

    Remove support for Python 3.8
    Its EOL was Oct 2024
    
    https://discuss.python.org/t/python-3-8-is-now-officially-eol/66983

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         "Intended Audience :: Information Technology",
         "Intended Audience :: System Administrators",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
