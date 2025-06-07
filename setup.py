c24a025e186ccf11f20b8f5959bbbdff53fbdcce
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2025-06-07 19:17:29 +0300

    Add support for Python 3.13

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
