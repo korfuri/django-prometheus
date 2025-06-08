f3f0ca154f0de12e31bbb7f52c77f1f018962f6a
Ülgen Sarıkavak <ulgens@users.noreply.github.com>
2025-06-08 16:33:25 +0300

    Add support for Django 5.2 (#459)

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.0",
         "Framework :: Django :: 5.1",
+        "Framework :: Django :: 5.2",
         "Topic :: System :: Monitoring",
         "License :: OSI Approved :: Apache Software License",
     ],
